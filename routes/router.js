befa5f66b71bb4496d001d96d1856d19bd0c2e25
psycho-valker <rameez.connect@gmail.com>
2018-04-29 13:42:06 +0530

    password created

--- routes/router.js
+++ routes/router.js
@@ -12,28 +12,25 @@ router.get('/', function (req, res, next) {
 //POST route for updating data
 router.post('/', function (req, res, next) {
   // confirm that user typed same password twice
-  if (req.body.password !== req.body.passwordConf) {
-    var err = new Error('Passwords do not match.');
-    err.status = 400;
-    res.send("passwords dont match");
-    return next(err);
-  }
+  // if (req.body.password !== req.body.passwordConf) {
+  //   var err = new Error('Passwords do not match.');
+  //   err.status = 400;
+  //   res.send("passwords dont match");
+  //   return next(err);
+  // }
 
   if (req.body.email &&
     req.body.username &&
-    req.body.password &&
-    req.body.passwordConf) {
+    req.body.password ) {
 
     var userData = {
       email: req.body.email,
       username: req.body.username,
       password: req.body.password,
-      passwordConf: req.body.passwordConf,
     }
 
     User.create(userData, function (error, user) {
       if (error) {
-        alert(error);
         return next(error);
       } else {
         req.session.userId = user._id;
