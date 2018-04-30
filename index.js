15eb815e0742bcd7e7b31ded1cc6331c4b01ce34
psycho-valker <rameez.connect@gmail.com>
2018-04-30 19:43:06 +0530

    mlab is yes done pass on mlab yes is good

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var session = require('express-session');
+var session = require('cookie-session');
 var MongoStore = require('connect-mongo')(session);
 var mongodb = require('mongodb');
 
